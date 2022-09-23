caaa1dfa255ffe9c9c9a1200b058b182f6aad079
Надежда Худякова <105661918+nadegda150791@users.noreply.github.com>
2022-09-23 18:55:16 +0400

    Добавляет js

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,22 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.body;
 
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+  if (body.classList.contains('no-js')) {
+    body.classList.remove('no-js')
   }
-});
+
+  let navMain = document.querySelector('.main-nav');
+  let navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+})
